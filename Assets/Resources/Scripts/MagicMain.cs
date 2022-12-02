b377f9e1163e6acb6ed386f3cc3693f5dcc09d44
HonkerForce <scorpiowolf@qq.com>
2022-12-02 18:28:33 +0800

    新增对线渲染器和拖尾渲染器的尝试
    调整了文件夹结构

--- Assets/Resources/Scripts/MagicMain.cs
+++ Assets/Resources/Scripts/MagicMain.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.Events;
 
 public class MagicMain : MonoBehaviour
 {
@@ -9,39 +10,37 @@ public class MagicMain : MonoBehaviour
 	// [SerializeField] private bool mCanRotated = false;
 	[SerializeField] private float mMoveSpeed = 0f;
 	[SerializeField] private float mRotateSpeed = 0f;
+	[SerializeField] private float mJumpHeight = 0f;
+
+	new public Rigidbody rigidbody;
 
 	public ObjectPool<GameObject> objPool;
 
-	// public bool IsRotated
-	// {
-	// 	get { return mCanRotated; }
-    //     set { mCanRotated = value; }
-	// }
+	public UnityEvent<int, int> triggerEvent;
 
 	public float MoveSpeed
 	{
-		get { return mMoveSpeed; }
-		set { mMoveSpeed = value; }
+		get => mMoveSpeed;
 	}
 
 	public float RotationSpeed
 	{
-		get { return mRotateSpeed; }
-		set { mRotateSpeed = value; }
+		get => mRotateSpeed;
 	}
 
 	public void PlayerRotate(Vector3 vecDict, float fAngle)
 	{
-		transform.Rotate(vecDict, fAngle);
+		transform?.Rotate(vecDict, fAngle);
 	}
 
 	public void PlayerMove(Vector3 vecDict, float fMove)
 	{
-		transform.Translate(vecDict * fMove);
+		transform?.Translate(vecDict * fMove);
 	}
 
 	public void SetPlayerEulerRotation(Vector3 vecRotation)
 	{
+		Debug.Log(vecRotation);
 		transform.rotation = Quaternion.Euler(vecRotation);
 	}
 
@@ -55,45 +54,32 @@ public class MagicMain : MonoBehaviour
 		    var go = objPool.Get();
 		    go.name = "DDY" + i;
 	    }
+
+		triggerEvent?.AddListener((a, b) => {Debug.Log(a + b);});
+		triggerEvent?.Invoke(1, 2);
     }
 
     void Update()
     {
-		if (Input.GetKey(KeyCode.W))
+		if(Input.GetKey(KeyCode.Escape))
 		{
-			// SetPlayerEulerRotation(new Vector3(RotationSpeed, 0, 0));
-			PlayerMove(Vector3.forward, MoveSpeed * Time.deltaTime);
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
 		}
-		else if(Input.GetKeyUp(KeyCode.W))
+		else if(Input.GetMouseButtonDown(0))
 		{
-			SetPlayerEulerRotation(Vector3.zero);
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
 		}
-		else if(Input.GetKey(KeyCode.S))
-		{
-			// SetPlayerEulerRotation(new Vector3(RotationSpeed, 180, 0));
-			PlayerMove(Vector3.back, MoveSpeed * Time.deltaTime);
-		}
-		else if(Input.GetKeyUp(KeyCode.S))
-		{
-			SetPlayerEulerRotation(Vector3.zero);
-		}
-		else if(Input.GetKey(KeyCode.A))
-		{
-			// SetPlayerEulerRotation(new Vector3(RotationSpeed, -90, 0));
-			PlayerMove(Vector3.left, MoveSpeed * Time.deltaTime);
-		}
-		else if(Input.GetKeyUp(KeyCode.A))
-		{
-			SetPlayerEulerRotation(Vector3.zero);
-		}
-		else if(Input.GetKey(KeyCode.D))
-		{
-			// SetPlayerEulerRotation(new Vector3(RotationSpeed, 90, 0));
-			PlayerMove(Vector3.right, MoveSpeed * Time.deltaTime);
-		}
-		else if(Input.GetKeyUp(KeyCode.D))
-		{
-			SetPlayerEulerRotation(Vector3.zero);
-		}
+
+		float horizontalMove = Input.GetAxis("Horizontal");
+		float verticalMove = Input.GetAxis("Vertical");
+		transform.Translate(new Vector3(horizontalMove, 0, verticalMove) * MoveSpeed * Time.deltaTime);
+
+		float horizontalRotate = Input.GetAxis("Mouse X");
+		transform.Rotate(new Vector3(0, horizontalRotate, 0) * RotationSpeed * Time.deltaTime);
+
+		float jumpMove = Input.GetAxis("Jump");
+		rigidbody?.AddForce(new Vector3(0, jumpMove, 0) * mJumpHeight * Time.deltaTime, ForceMode.Impulse);
     }
 }
